95f36240b7c4ae95f9305aea5390cb77e0a2370f
HateMyLife <ivan.vasyliv0123@gmail.com>
2019-03-16 07:33:28 +0200

    Committing a finalized product.

---
+++ Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp1
+{
+    class Person
+    {
+        private string name;
+        private string surname;
+        private string email;
+        private string birthdate;
+        private readonly bool isAdult;
+        private readonly bool isBirthday;
+        private readonly string chineseSign;
+        private readonly string sunSign;
+        private int[] sunSignChangeDays = new int[12]
+        {
+            21,21,21,21,21,22,23,24,23,24,23,22
+        };
+        private string[] chineseSigns = new string[12]
+        {
+            "Rat", "Ox" , "Tiger", "Rabbit", "Dragon", "Snake",
+        "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"
+        };
+        string[] sunSigns = new string[12]
+        {
+            "Capricorn","Aquerius", "Pisces", "Aries", "Taurus" , "Gemini", "Cancer",
+            "Leo", "Virgo","Libra", "Scoprio", "Sagittarius"
+        };
+
+        public Person(string name, string surname, string email, DateTime birthdate)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.email = email;
+            this.birthdate = birthdate.ToShortDateString();
+            DateTime now = DateTime.Now;
+            isBirthday = now.Day == birthdate.Day && now.Month == birthdate.Month;
+            isAdult = now > birthdate.AddYears(18);
+            int inEuropian = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                if (birthdate.Month > i + 1 || (birthdate.Month == i + 1 && birthdate.Day >= sunSignChangeDays[i]))
+                {
+                    inEuropian = i + 1;
+                }
+            }
+            inEuropian %= 12;
+            int inChinese = (birthdate.Year - 4 - ((birthdate < new DateTime(birthdate.Year, 2, 5)) ? 1 : 0)) % 12;
+            chineseSign = chineseSigns[inChinese];
+            sunSign = sunSigns[inEuropian];
+            MessageBox.Show(this.print());
+        }
+        Person(string name, string surname, string email) : this(name, surname, email, DateTime.Now) { }
+        Person(string name, string surname, DateTime birthdate) : this(name, surname, null, birthdate) { }
+        public string print()
+        {
+            return "Name -- " + name + "\nSurname -- " + surname + "\nEmail -- " + email + "\nDate of birth -- " + birthdate +
+                "\nIs an adult? -- " + isAdult + "\nSelebrates birthday today? -- " + isBirthday +
+                "\nChinese Zodiac sign -- " + chineseSign + "\nSun Zodiac sign -- " + sunSign;
+        }
+    }
+}
---
+++ VM.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class VM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        string name;
+        string surname;
+        string email;
+        string birthdate;
+        private LogDataCommand getResult;
+        private Person entered;
+        public string Name { get => name; set
+            {
+                name = value;
+                NotifyPropertyChanged("Name");
+
+            }
+        }
+        public string Surname { get => surname; set
+            {
+                surname = value;
+                NotifyPropertyChanged("Surname");
+            }
+        }
+        public string Email { get => email; set
+            {
+                email = value;
+                NotifyPropertyChanged("Email");
+            }
+        }
+        public string Birthdate { get => birthdate; set
+            {
+                birthdate = value;
+                NotifyPropertyChanged("Birthdate");
+            }
+        }
+        internal Person Entered { get => entered; set => entered = value; }
+        public LogDataCommand GetResult { get => getResult; set => getResult = value; }
+
+        public VM()
+        {
+            GetResult = new LogDataCommand(this);
+        }
+    }
+    public class LogDataCommand : ICommand
+    {
+        VM parent;
+
+        public LogDataCommand(VM parent)
+        {
+            this.parent = parent;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            if (string.IsNullOrEmpty(parent.Name)) return false;
+            if (string.IsNullOrEmpty(parent.Surname)) return false;
+            if (string.IsNullOrEmpty(parent.Email)) return false;
+            if (string.IsNullOrEmpty(parent.Birthdate)) return false;
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            DateTime selDate;
+            try
+            {
+                if (!DateTime.TryParse(parent.Birthdate, out selDate))
+                    throw new FormatException("String does not represent a valid date");
+                TimeSpan dif = DateTime.Now - selDate;
+                if (DateTime.Now.AddDays(dif.TotalDays) < DateTime.Now.AddYears(-120) || dif.Seconds < 0)
+                    throw new ArgumentOutOfRangeException("Date should not be more than 120 years in the past or in the future");
+                if (!IsValidEmail(parent.Email)) throw new ArgumentException("Invalid email address");
+                parent.Entered = new Person(parent.Name, parent.Surname, parent.Email, selDate);
+            }
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show(e.Message);
+            }
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
